c7a053336fae8db306658850a23e452d9f2b78bb
Ishan-Gunaratne <ramindu.ishan99@hotmail.com>
2019-10-19 14:11:53 +0530

    [Feat]: Add js types Create javascript types with examples, String,Numbers Boolean etc....

---
+++ types.js
@@ -0,0 +1,81 @@
+
+
+/*
+    🌀Primitive types,
+    1.Numbers
+    2.Strings
+    3.Booleans
+
+    🌀Special types 
+    1.null
+    2.undefine
+
+    🌀Numbers
+    Integers - 10 , 3233433
+    floats - 3.14 , .1234
+
+     🌀Strings
+    String typ is Sequence Charaters
+    enclose in quotes or double quotes
+
+    string can span across multiple line by using the
+    backslash
+Ex :- ⏮
+    "A \
+     string"
+*/
+//Ex :- ⏮
+ var a = "name of the string" 
+    var b = "A \
+    name of the string "
+    console.log(b);
+
+
+// 🌀Template strings
+//Ex :- ⏮
+
+myobj = {
+    a: "Ishan"
+}
+
+console.log(`my name is ${myobj.a}`)
+
+//Passing the object to the string 
+
+
+// 🌀Booleans
+
+//JavaScript defines two reserved words for booleans: true and false
+//Ex :- ⏮
+var a = undefined
+console.log(a);
+
+
+// 🌀null
+
+// null is the special value indicates the absence of value.
+//Ex :- ⏮
+var b = null
+console.log(b);
+
+
+
+// 🌀Undefined 
+
+// To detect if a value is
+// undefined
+// , you use the construct:
+
+// type of variable === 'undefined'
+
+// It's commonly returned by functions with no return values.
+
+
+// 🌀Object Types
+//Ex :- ⏮
+var num = {
+    a:1,
+    b:3,
+    c:4
+}
+console.log(num);
