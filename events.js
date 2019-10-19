2fae5ceb39ca9f759446a088a90e138e74174a24
Sanoj Silva <silvasanoj@gmail.com>
2019-10-19 20:38:16 +0530

    [Feat]: Add js events

---
+++ events.js
@@ -0,0 +1,15 @@
+// we use event handlers to handle events
+
+// 👉 inline event handlers
+// <button onclick="doSomething()">my button</button>
+// this means when we click the button call the function doSomething()
+
+// 👉 DOM on-event handlers
+// window.onload = () => {
+//     console.log("runs when the window loads");
+// };
+
+// 👉 using addEventListener()
+// window.addEventListener("load", () => {
+//     console.log("window loaded");
+// });
